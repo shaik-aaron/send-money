d1b67d3e60f7507a42207859221fdd6a36daebfb
Sooraj M Singh <sooraj.singh@leafround.com>
2023-07-18 21:39:37 +0530

    fixed css issues

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -5,24 +5,22 @@ import currencies from "../data/currencies";
 import transactions from "../data/transactions";
 
 function Main() {
-    return (
-        <main>
-            <p className="your--account">Your Account</p>
-            <div id="currency--container">
-                {currencies.map((currency) => {
-                    return <Currency key={currency.id} {...currency} />;
-                })}
-            </div>
-            <div id="transactions">
-                <p className="transactions--text">Transactions</p>
-                {transactions.map((transaction) => {
-                    return (
-                        <Transaction key={transaction.id} {...transaction} />
-                    );
-                })}
-            </div>
-        </main>
-    );
+  return (
+    <main>
+      <p className="your--account">Your Account</p>
+      <div className={"cards-container"} id="currency--container">
+        {currencies.map((currency) => {
+          return <Currency key={currency.id} {...currency} />;
+        })}
+      </div>
+      <div id="transactions">
+        <p className="transactions--text">Transactions</p>
+        {transactions.map((transaction) => {
+          return <Transaction key={transaction.id} {...transaction} />;
+        })}
+      </div>
+    </main>
+  );
 }
 
 export default Main;
